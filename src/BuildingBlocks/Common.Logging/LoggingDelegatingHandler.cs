597190c4bf1ea977e3e68d328478b1156f197dde
INNO-IT <INNO-IT@INNO-OMEN1>
2022-02-19 20:30:08 +0100

    Fixes

--- src/BuildingBlocks/Common.Logging/LoggingDelegatingHandler.cs
+++ src/BuildingBlocks/Common.Logging/LoggingDelegatingHandler.cs
@@ -1,47 +1,52 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
-namespace Common.Logging;
-
-public class LoggingDelegatingHandler : DelegatingHandler
+namespace Common.Logging
 {
-    private readonly ILogger<LoggingDelegatingHandler> _logger;
-
-    public LoggingDelegatingHandler(ILogger<LoggingDelegatingHandler> logger)
+    public class LoggingDelegatingHandler : DelegatingHandler
     {
-        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-    }
+        private readonly ILogger<LoggingDelegatingHandler> _logger;
 
-    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-    {
-        try
+        public LoggingDelegatingHandler(ILogger<LoggingDelegatingHandler> logger)
         {
-            var response = await base.SendAsync(request, cancellationToken);
-            
-            if (response.IsSuccessStatusCode)
-                _logger.LogInformation("Received a success response from {Url}", response.RequestMessage.RequestUri);
-            else
-                _logger.LogWarning("Received a non-success status code {StatusCode} from {Uri}",
-                    (int)response.StatusCode, response.RequestMessage.RequestUri);
-
-            return response;
-        }
-        catch (HttpRequestException e)
-            when (e.InnerException is SocketException {SocketErrorCode: SocketError.ConnectionRefused})
-        {
-            var hostWithPort = request.RequestUri.IsDefaultPort
-                ? request.RequestUri.DnsSafeHost
-                : $"{request.RequestUri.DnsSafeHost}:{request.RequestUri.Port}";
-            
-            _logger.LogCritical(e, "Unable to connect to {Host}. Please check the " +
-                                   "configuration to ensure the correct URL for the service " +
-                                   "has been configured.", hostWithPort);
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
-        return new HttpResponseMessage(HttpStatusCode.BadGateway)
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            RequestMessage = request
-        };
+            try
+            {
+                var response = await base.SendAsync(request, cancellationToken);
+            
+                if (response.IsSuccessStatusCode)
+                    _logger.LogInformation("Received a success response from {Url}", response.RequestMessage.RequestUri);
+                else
+                    _logger.LogWarning("Received a non-success status code {StatusCode} from {Uri}",
+                        (int)response.StatusCode, response.RequestMessage.RequestUri);
+
+                return response;
+            }
+            catch (HttpRequestException e)
+                when (e.InnerException is SocketException {SocketErrorCode: SocketError.ConnectionRefused})
+            {
+                var hostWithPort = request.RequestUri.IsDefaultPort
+                    ? request.RequestUri.DnsSafeHost
+                    : $"{request.RequestUri.DnsSafeHost}:{request.RequestUri.Port}";
+            
+                _logger.LogCritical(e, "Unable to connect to {Host}. Please check the " +
+                                       "configuration to ensure the correct URL for the service " +
+                                       "has been configured.", hostWithPort);
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.BadGateway)
+            {
+                RequestMessage = request
+            };
+        }
     }
 }
