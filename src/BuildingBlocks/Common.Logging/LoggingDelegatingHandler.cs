b2d01418762e0a404595779c205a95fc1086076d
INNO-IT <INNO-IT@INNO-OMEN1>
2022-02-19 19:07:49 +0100

    Common.Logging: Add HttpClient DelegatingHandler for logging httpClient communications

---
+++ src/BuildingBlocks/Common.Logging/LoggingDelegatingHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using Microsoft.Extensions.Logging;
+
+namespace Common.Logging;
+
+public class LoggingDelegatingHandler : DelegatingHandler
+{
+    private readonly ILogger<LoggingDelegatingHandler> _logger;
+
+    public LoggingDelegatingHandler(ILogger<LoggingDelegatingHandler> logger)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var response = await base.SendAsync(request, cancellationToken);
+            
+            if (response.IsSuccessStatusCode)
+                _logger.LogInformation("Received a success response from {Url}", response.RequestMessage.RequestUri);
+            else
+                _logger.LogWarning("Received a non-success status code {StatusCode} from {Uri}",
+                    (int)response.StatusCode, response.RequestMessage.RequestUri);
+
+            return response;
+        }
+        catch (HttpRequestException e)
+            when (e.InnerException is SocketException {SocketErrorCode: SocketError.ConnectionRefused})
+        {
+            var hostWithPort = request.RequestUri.IsDefaultPort
+                ? request.RequestUri.DnsSafeHost
+                : $"{request.RequestUri.DnsSafeHost}:{request.RequestUri.Port}";
+            
+            _logger.LogCritical(e, "Unable to connect to {Host}. Please check the " +
+                                   "configuration to ensure the correct URL for the service " +
+                                   "has been configured.", hostWithPort);
+        }
+
+        return new HttpResponseMessage(HttpStatusCode.BadGateway)
+        {
+            RequestMessage = request
+        };
+    }
+}
